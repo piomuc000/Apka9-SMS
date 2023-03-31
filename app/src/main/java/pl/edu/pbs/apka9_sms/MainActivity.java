291d354afc27aafb5200bfd85f3a436d8eeea97f
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-03-31 14:29:54 +0200

    Initial commit

---
+++ app/src/main/java/pl/edu/pbs/apka9_sms/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.edu.pbs.apka9_sms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
